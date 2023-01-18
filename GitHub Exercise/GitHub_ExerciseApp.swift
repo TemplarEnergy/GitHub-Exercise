591f8a30d8946347c8d9f8690b3a61b29dedf6a9
Tom Radford <tom@radfords-web.com>
2023-01-17 23:10:17 -0400

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Thomas Radford on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
